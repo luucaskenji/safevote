d5f7309e73186db54199748a89dd939bd67940e3
Lucas Murakami <lkenjim98@gmail.com>
2021-01-04 18:12:13 -0400

    POST vote route: using "cpf" lib to validate incoming data

--- src/controllers/votesController.js
+++ src/controllers/votesController.js
@@ -1,11 +1,14 @@
 const voteSchema = require('../schemas/votes');
+const { validate } = require('gerador-validador-cpf');
 
 function postVote(req, res) {
     const { candidateNumber, cpf } = req.body;
     
     if (!candidateNumber || !cpf) return res.status(400).send('Fields missing');
 
-    if (voteSchema.validate(req.body).error) return res.status(422).send('Data in wrong pattern');
+    if (voteSchema.validate(req.body).error || !validate(req.body.cpf)) {
+        return res.status(422).send('Data in wrong pattern');
+    }
 
     res.status(200).send('OK');
 }
