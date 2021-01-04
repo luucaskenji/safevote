068e6fe64e5a0e78168d5d3f5407119eac616a3e
Lucas Murakami <lkenjim98@gmail.com>
2021-01-04 17:31:33 -0400

    app preparation

--- src/app.js
+++ src/app.js
@@ -3,6 +3,10 @@ dotenv.config();
 
 const express = require("express");
 const app = express();
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
 
 // Remova essa rota
 app.use("*", (req, res) => res.sendStatus(501));
